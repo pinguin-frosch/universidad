cf194dd3ee7c830dc4e6f13195a03aa61dafb758
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-07-10 18:35:41 -0400

    Ejercicios 9 y 10 bases de datos Charlotte

---
+++ tercer_semestre/omg 🐈/bases de datos/datos.js
@@ -0,0 +1,269 @@
+db.alumnos.insertMany([
+  {
+    "nombre": "Luis",
+    "cursos": [
+      {
+        "tema": "PHP",
+        "nota": 4
+      },
+      {
+        "tema": "C#",
+        "nota": 6
+      },
+      {
+        "tema": "Cobol",
+        "nota": 7
+      },
+      {
+        "tema": "PYTHON",
+        "nota": 5
+      }
+    ],
+    "edad": 25,
+    "ciudad": [
+      {
+        "nombre": "Concepcion",
+        "tiempo": 16
+      }
+    ]
+  },
+  {
+    "nombre": "Pablo",
+    "cursos": [
+      {
+        "tema": "C#",
+        "nota": 7
+      },
+      {
+        "tema": "PYTHON",
+        "nota": 6
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      }
+    ],
+    "edad": 27,
+    "ciudad": [
+      {
+        "nombre": "Chillán",
+        "tiempo": 16
+      }
+    ]
+  },
+  {
+    "nombre": "Maria",
+    "cursos": [
+      {
+        "tema": "PHP",
+        "nota": 2
+      },
+      {
+        "tema": "JAVA",
+        "nota": 4
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      }
+    ],
+    "edad": 30,
+    "ciudad": [
+      {
+        "nombre": "Concepcion",
+        "tiempo": 16
+      },
+      {
+        "nombre": "Temuco",
+        "tiempo": 10
+      }
+    ]
+  },
+  {
+    "nombre": "Ana",
+    "cursos": [
+      {
+        "tema": "JAVA",
+        "nota": 3
+      },
+      {
+        "tema": "JS",
+        "nota": 5
+      },
+      {
+        "tema": "PHP",
+        "nota": 4
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      }
+    ],
+    "edad": 35,
+    "Ciudad": "Talcahuano"
+  },
+  {
+    "nombre": "Matias",
+    "cursos": [
+      {
+        "tema": "JS",
+        "nota": 4
+      },
+      {
+        "tema": "PHP",
+        "nota": 6
+      },
+      {
+        "tema": "C#",
+        "nota": 6
+      }
+    ],
+    "edad": 26,
+    "ciudad": "Concepcion"
+  },
+  {
+    "nombre": "Mariano",
+    "cursos": [
+      {
+        "tema": "JAVA",
+        "nota": 7
+      },
+      {
+        "tema": "PHP",
+        "nota": 5
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      },
+      {
+        "tema": "PYTHON",
+        "nota": 4
+      }
+    ],
+    "edad": 28,
+    "ciudad": "Concepción"
+  },
+  {
+    "nombre": "Sofia",
+    "cursos": [
+      {
+        "tema": "PHP",
+        "nota": 7
+      },
+      {
+        "tema": "C#",
+        "nota": 6
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      }
+    ],
+    "edad": 26,
+    "ciudad": [
+      {
+        "nombre": "Chillan",
+        "tiempo": 12
+      },
+      {
+        "nombre": "Temuco",
+        "tiempo": 9
+      }
+    ]
+  },
+  {
+    "nombre": "Marcelo",
+    "cursos": [
+      {
+        "tema": "C#",
+        "nota": 6
+      },
+      {
+        "tema": "JAVA",
+        "nota": 5
+      },
+      {
+        "tema": "JS",
+        "nota": 4
+      }
+    ],
+    "edad": 23,
+    "ciudad": "Chillan"
+  },
+  {
+    "nombre": "Sonia",
+    "cursos": [
+      {
+        "tema": "PHP",
+        "nota": 4
+      },
+      {
+        "tema": "JAVA",
+        "nota": 4
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      }
+    ],
+    "edad": 27,
+    "ciudad": [
+      {
+        "nombre": "Concepcion",
+        "tiempo": 6
+      },
+      {
+        "nombre": "Temuco",
+        "tiempo": 4
+      }
+    ]
+  },
+  {
+    "nombre": "Sonia",
+    "cursos": [
+      {
+        "tema": "JAVA",
+        "nota": 7
+      },
+      {
+        "tema": "JS",
+        "nota": 5
+      },
+      {
+        "tema": "PYTHON",
+        "nota": 4
+      },
+      {
+        "tema": "Cobol",
+        "nota": 4
+      }
+    ],
+    "edad": 23,
+    "ciudad": "Concepción"
+  },
+  {
+    "nombre": "Julia",
+    "cursos": [
+      {
+        "tema": "PYTHON",
+        "nota": 7
+      },
+      {
+        "tema": "PYTHON",
+        "nota": 7
+      }
+    ],
+    "edad": 24,
+    "ciudad": [
+      {
+        "nombre": "Chillán",
+        "tiempo": 16
+      },
+      {
+        "nombre": "Temuco",
+        "tiempo": 10
+      }
+    ]
+  }
+])
