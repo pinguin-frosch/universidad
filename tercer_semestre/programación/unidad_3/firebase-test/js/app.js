d2149d642430a4d352db8d2f15b0f87985b1736b
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-07-10 18:39:24 -0400

    Corrección de firebase y los métodos

--- tercer_semestre/programación/unidad_3/firebase-test/js/app.js
+++ tercer_semestre/programación/unidad_3/firebase-test/js/app.js
@@ -6,13 +6,14 @@ import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.8.4/
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBWvB9LR97aLNaVZ_qiH7TnZnshUhN3RcE',
-  authDomain: 'empleadosgb-f1d6e.firebaseapp.com',
-  projectId: 'empleadosgb-f1d6e',
-  storageBucket: 'empleadosgb-f1d6e.appspot.com',
-  messagingSenderId: '1091063224428',
-  appId: '1:1091063224428:web:473be1c604c500d9a6e049'
-}
+  // apiKey: "AIzaSyBWvB9LR97aLNaVZ_qiH7TnZnshUhN3RcE",
+  // authDomain: "empleadosgb-f1d6e.firebaseapp.com",
+  databaseURL: "https://empleadosgb-f1d6e-default-rtdb.firebaseio.com",
+  // projectId: "empleadosgb-f1d6e",
+  // storageBucket: "empleadosgb-f1d6e.appspot.com",
+  // messagingSenderId: "1091063224428",
+  // appId: "1:1091063224428:web:473be1c604c500d9a6e049"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
@@ -20,20 +21,19 @@ const app = initializeApp(firebaseConfig)
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app)
 
-const agregar_empleado = () => {
-  let rut_empleado = document.getElementById('rut').value
-  let nombre = document.getElementById('nombre').value
-  let email = document.getElementById('email').value
-  let telefono = document.getElementById('telefono').value
-  let foto = document.getElementById('foto').value
-  // set(ref(database, `empleados/${rut_empleado}`), {
-  //   nombre: nombre,
-  //   email: email,
-  //   telefono: telefono,
-  //   url_foto: foto
-  // })
-}
-
-const button = document.getElementById('agregarEmp')
-button.addEventListener('click', agregar_empleado)
+const button = document.getElementById('agregar_empleado')
+button.addEventListener('click', () => {
+    let rut = document.getElementById('rut').value
+    let nombre = document.getElementById('nombre').value
+    let email = document.getElementById('email').value
+    let telefono = document.getElementById('telefono').value
+    let foto = document.getElementById('foto').value
+    set(ref(database, 'empleados/' + rut), {
+        rut: rut,
+        nombre: nombre,
+        email: email,
+        telefono: telefono,
+        foto: foto
+    })
+})
 
