671ffff25303cb9e649f34bccfa2da94c53aec53
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-09-08 11:32:34 -0400

    Ejercios de java simples

---
+++ cuarto_semestre/aplicaciones_mobiles/unidad_1/java/tareas/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i = 5;
+        while (i <= 35) {
+            System.out.println(i);
+            i += 5;
+        }
+
+        for (int j = 5; j <= 35; j += 5) {
+            System.out.println(j);
+        }
+    }
+}
