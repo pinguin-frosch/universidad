d6ace3f55f4a7c3a1ac4fa3cfd06f78ef5afc2a9
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-09-09 15:03:15 -0400

    Ahora funcionan todas las operaciones

--- cuarto_semestre/aplicaciones_mobiles/unidad_1/Android Studio/OperacionesMatematicas/app/src/main/java/com/example/operacionesmatematicas/MainActivity.java
+++ cuarto_semestre/aplicaciones_mobiles/unidad_1/Android Studio/OperacionesMatematicas/app/src/main/java/com/example/operacionesmatematicas/MainActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // Declarar los elementos para que sean globales dentro de la clase
-    EditText num1, num2;
-    TextView res;
+    private EditText num1, num2;
+    private TextView res;
+    private RadioButton sumar, restar, multiplicar, dividir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +24,39 @@ public class MainActivity extends AppCompatActivity {
         num1 = findViewById(R.id.etn_num1);
         num2 = findViewById(R.id.etn_num2);
         res = findViewById(R.id.tv_res);
+        sumar = findViewById(R.id.rb_sumar);
+        restar = findViewById(R.id.rb_restar);
+        dividir = findViewById(R.id.rb_dividir);
+        multiplicar = findViewById(R.id.rb_multiplicar);
     }
 
-    public void sumar(View view) {
+    public void operar(View view) {
         // Obtener el texto de los inputs
         String num1text = num1.getText().toString();
         String num2text = num2.getText().toString();
 
-        // Si alguno está vació no haremos nada
-        if (num1text.equals("") || num2text.equals("")) {
-            return;
-        }
-
-        // Realizar la suma
         int numero1 = Integer.parseInt(num1text);
         int numero2 = Integer.parseInt(num2text);
-        int suma = numero1 + numero2;
+
+        int resultado;
+
+        if (sumar.isChecked()) {
+            // Sumar
+            resultado = numero1 + numero2;
+        } else if (restar.isChecked()) {
+            // Restar
+            resultado = numero1 - numero2;
+        } else if (multiplicar.isChecked()) {
+            // Multiplicar
+            resultado = numero1 * numero2;
+        } else if (dividir.isChecked()) {
+            // Dividir
+            resultado = numero1 / numero2;
+        } else {
+            resultado = 0;
+        }
 
         // Actualizar el resultado con la suma
-        res.setText(String.valueOf(suma));
-        res.setText(String.format("%d + %d = %d", numero1, numero2, suma));
+        res.setText(String.valueOf(resultado));
     }
 }
