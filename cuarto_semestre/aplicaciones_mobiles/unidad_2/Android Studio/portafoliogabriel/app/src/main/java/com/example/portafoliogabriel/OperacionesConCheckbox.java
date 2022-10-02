1e6df746d452018037e0f10650586767670c9ecb
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-10-02 19:58:49 -0300

    Aplicación operaciones con checkbox

--- cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/OperacionesConCheckbox.java
+++ cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/OperacionesConCheckbox.java
@@ -2,13 +2,58 @@ package com.example.portafoliogabriel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class OperacionesConCheckbox extends AppCompatActivity {
+
+    EditText editTextNumero1, editTextNumero2;
+    CheckBox checkBoxSumar, checkBoxRestar;
+    TextView textViewResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operaciones_con_checkbox);
+
+        editTextNumero1 = findViewById(R.id.ochEditTextNumero1);
+        editTextNumero2 = findViewById(R.id.ochEditTextNumero2);
+        checkBoxSumar = findViewById(R.id.ochCheckBoxSumar);
+        checkBoxRestar = findViewById(R.id.ochCheckBoxRestar);
+        textViewResultado = findViewById(R.id.ochTextViewResultado);
+    }
+
+    @SuppressLint("DefaultLocale")
+    public void operar(View view) {
+        String stringNumero1 = editTextNumero1.getText().toString();
+        String stringNumero2 = editTextNumero2.getText().toString();
+
+        if (stringNumero1.equals("") || stringNumero2.equals("")) {
+            return;
+        }
+
+        int intNumero1 = Integer.parseInt(stringNumero1);
+        int intNumero2 = Integer.parseInt(stringNumero2);
+        String stringResultado;
+
+        if (checkBoxSumar.isChecked() && checkBoxRestar.isChecked()) {
+            int suma = intNumero1 + intNumero2;
+            int resta = intNumero1 - intNumero2;
+            stringResultado = String.format("%d + %d = %d, %d - %d = %d", intNumero1, intNumero2, suma, intNumero1, intNumero2, resta);
+        } else if (checkBoxSumar.isChecked()) {
+            int suma = intNumero1 + intNumero2;
+            stringResultado = String.format("%d + %d = %d", intNumero1, intNumero2, suma);
+        } else if (checkBoxRestar.isChecked()) {
+            int resta = intNumero1 - intNumero2;
+            stringResultado = String.format("%d - %d = %d", intNumero1, intNumero2, resta);
+        } else {
+            return;
+        }
+
+        textViewResultado.setText(stringResultado);
     }
 }
