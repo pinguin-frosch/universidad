050314a99f27f725048cb2ac8d57d209e6618cf6
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-10-02 18:53:08 -0300

    Aplicación signo

--- cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/Signo.java
+++ cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/Signo.java
@@ -3,12 +3,43 @@ package com.example.portafoliogabriel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Signo extends AppCompatActivity {
+
+    EditText editTextNumero;
+    TextView textViewSigno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signo);
+
+        editTextNumero = findViewById(R.id.editTextNumero);
+        textViewSigno = findViewById(R.id.textViewSigno);
+    }
+
+    public void determinarSigno(View view) {
+        String stringNumero = editTextNumero.getText().toString();
+
+        if (stringNumero.equals("")) {
+            return;
+        }
+
+        int intNumero = Integer.parseInt(stringNumero);
+
+        String stringSigno;
+
+        if (intNumero > 0) {
+            stringSigno = "Positivo";
+        } else if (intNumero < 0) {
+            stringSigno = "Negativo";
+        } else {
+            stringSigno = "Neutro";
+        }
+
+        textViewSigno.setText(stringSigno);
     }
 }
