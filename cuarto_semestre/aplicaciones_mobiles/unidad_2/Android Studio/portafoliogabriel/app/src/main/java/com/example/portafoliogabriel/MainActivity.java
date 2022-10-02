a55d6cbaea577f2f77339d337db22a0a10223cec
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-10-02 16:30:06 -0300

    Menú principal terminado

--- cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/MainActivity.java
+++ cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.portafoliogabriel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void iniciarSumar(View view) {
+        Intent i = new Intent(this, Sumar.class);
+        startActivity(i);
+    }
+
+    public void iniciarOperacionesConRadio(View view) {
+        Intent i = new Intent(this, OperacionesConRadio.class);
+        startActivity(i);
+    }
+
+    public void iniciarSigno(View view) {
+        Intent i = new Intent(this, Signo.class);
+        startActivity(i);
+    }
+
+    public void iniciarOperacionesConCheckbox(View view) {
+        Intent i = new Intent(this, OperacionesConCheckbox.class);
+        startActivity(i);
     }
 }
---
+++ cuarto_semestre/aplicaciones_mobiles/unidad_2/Android Studio/portafoliogabriel/app/src/main/java/com/example/portafoliogabriel/OperacionesConCheckbox.java
@@ -0,0 +1,14 @@
+package com.example.portafoliogabriel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OperacionesConCheckbox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operaciones_con_checkbox);
+    }
+}
