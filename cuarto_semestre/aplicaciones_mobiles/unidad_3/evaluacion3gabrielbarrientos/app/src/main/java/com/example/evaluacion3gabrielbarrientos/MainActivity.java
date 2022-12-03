7f5cf2d1ae33c79796b025758fc4c4c7b78f5eb8
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-12-02 23:17:59 -0300

    Diseño de la interfaz de la aplicación

---
+++ cuarto_semestre/aplicaciones_mobiles/unidad_3/evaluacion3gabrielbarrientos/app/src/main/java/com/example/evaluacion3gabrielbarrientos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluacion3gabrielbarrientos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
