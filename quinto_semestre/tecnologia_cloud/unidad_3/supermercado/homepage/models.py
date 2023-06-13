7b9b04ec5283504e2e8e3e452bbad8b7983bf006
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-06-13 10:20:45 -0400

    Modelos y página inicial actualizada

--- quinto_semestre/tecnologia_cloud/unidad_3/supermercado/homepage/models.py
+++ quinto_semestre/tecnologia_cloud/unidad_3/supermercado/homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    precio = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
