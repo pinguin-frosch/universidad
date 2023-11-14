0ae64c0f389cbb6325d257b43d2e833a7efcfbc1
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-11-14 20:10:24 -0300

    Completar trabajo con matrices y función para graficar

---
+++ sexto_semestre/mineria/unidad_2/evaluacion/main.r
@@ -0,0 +1,64 @@
+# Definir empresas y meses de operación
+empresas <- c("Aranguiz", "NCN", "Sanhueza", "Valroa")
+meses <- c("Enero", "Febrero", "Marzo", "Abril")
+
+# Ingresos de empresas por meses en el recorrido Santa Bárbara -> Los Ángeles
+# |        |Enero |Febrero|Marzo |Abril |
+# |Aranguiz|170000|175550 |252300|260300|
+# |NCN     |172500|178050 |148250|151950|
+# |Sanhueza|71500 |73350  |102800|105250|
+# |Valroa  |350000|363500 |496500|513100|
+
+aranguiz <- c(170000, 175550, 252300, 260300)
+ncn <- c(172500, 178050, 148250, 151950)
+sanhueza <- c(71500, 73350, 102800, 105250)
+valroa <- c(350000, 363500, 496500, 513100)
+
+ingresos <- rbind(aranguiz, ncn, sanhueza, valroa)
+colnames(ingresos) <- meses
+rownames(ingresos) <- empresas
+
+graph <- function(data, sub_rows, sub_cols, scale = 10000) {
+  # Tomar solo un subconjunto de datos de la matriz
+  data <- data[sub_rows, sub_cols]
+  data <- data / scale
+
+  # Información principal del gráfico
+  matplot(
+    seq_along(colnames(data)),
+    t(data),
+    type = "b",
+    col = c(1, 2, 4, 6),
+    pch = 15:20,
+    xlab = "Meses",
+    ylab = sprintf("Ingresos en montos de %d$", scale),
+    xaxt = "n",
+    yaxt = "n",
+    main = "Ingresos por empresa, primer cuatrimestre 2023."
+  )
+
+  # Agregar leyenda al gráfico
+  legend(
+    "topleft",
+    inset = 0.01,
+    legend = rownames(data),
+    pch = 15:20,
+    col = c(1, 2, 4, 6)
+  )
+
+  # Calcular los valores del eje y en la escala especificada
+  y_max <- ceiling(max(data))
+  y_step <- ceiling(y_max / 12)
+  y_labels <- seq(0, y_max, y_step)
+
+  # Agregar los ejes manualmente para usar los nombres correctos
+  axis(1, at = seq_along(colnames(data)), labels = colnames(data))
+  axis(2, at = y_labels, labels = y_labels, las = 1)
+}
+
+graph(
+  ingresos,
+  sub_cols = (meses != "Febrero"),
+  sub_rows = (empresas != "Valroa"),
+  scale = 25000
+)
